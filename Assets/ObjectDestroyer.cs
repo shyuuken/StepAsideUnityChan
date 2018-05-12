c5f50fcecb7cac778b24e10cfc1d10127f06b193
Taku Funami <fff273109@gmail.com>
2018-05-12 17:10:59 +0900

    Lesson6 課題再提出
    前回の提出の際にコミットし忘れていたと思われるアイテムの廃棄のコードを提出いたします。

---
+++ Assets/ObjectDestroyer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour {
+
+    //DestoryPlaneのオブジェクト
+    private GameObject DestoryPlane;
+
+    // Use this for initialization
+    void Start () {
+        //DestoryPlaneのオブジェクトを取得
+        this.DestoryPlane = GameObject.Find("DestoryPlane");
+    }
+
+    //トリガーモードで他のオブジェクトと接触したときの処理
+    void OnTriggerEnter(Collider other)
+    { 
+        //オブジェクトに衝突した場合
+        {
+            //接触したオブジェクトを破棄
+            Destroy(other.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+      
+		
+	}
+}
